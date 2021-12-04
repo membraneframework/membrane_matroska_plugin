1f8bc2793f3499957e3f79d4aeef54c538c02dd4
Maks Stachowiak <maks.stachowiak@swmansion.com>
2021-12-04 18:17:10 +0100

    refactored filtering active buffers

--- lib/membrane_webm/demuxer.ex
+++ lib/membrane_webm/demuxer.ex
@@ -52,8 +52,6 @@ defmodule Membrane.WebM.Demuxer do
 
   @impl true
   def handle_process(:input, %Buffer{payload: data, metadata: %{name: name}}, _context, state) do
-    # IO.puts("  Demuxer received #{name}")
-
     {actions, state} =
       case name do
         :Info ->
@@ -67,17 +65,12 @@ defmodule Membrane.WebM.Demuxer do
           {actions, %State{state | tracks: tracks}}
 
         :Cluster ->
-          # TODO You could create sending buffer's action and cache the buffers for inactive pads all at one pass. Just create maybe_send_buffers function where you will reduce the buffers and state.
-          buffers = cluster_to_buffers(data)
-          actions = Enum.map(active(buffers, state), &output/1)
+          {active, inactive} =
+            data
+            |> cluster_to_buffers
+            |> Enum.split_with(fn {id, _buffer} -> state.tracks[id].active end)
 
-          if actions != [] do
-            # IO.puts("    Demuxer sending Buffer")
-          end
-
-          to_cache = inactive(buffers, state)
-          new_cache = state.cache ++ to_cache
-          {actions, %State{state | cache: new_cache}}
+          {output(active), %State{state | cache: state.cache ++ inactive}}
 
         _ ->
           {[], state}
@@ -88,10 +81,8 @@ defmodule Membrane.WebM.Demuxer do
 
   @impl true
   def handle_pad_added(Pad.ref(:output, id), _context, %State{tracks: tracks} = state) do
-    track_info = Map.fetch!(tracks, id)
-
     caps =
-      case track_info.codec do
+      case tracks[id].codec do
         :opus -> %Opus{channels: 2, self_delimiting?: false}
         # TODO :opus -> %Opus{channels: track_info.channels, self_delimiting?: false}
         # TODO it's not a remote stream
@@ -100,34 +91,25 @@ defmodule Membrane.WebM.Demuxer do
         :vp9 -> %RemoteStream{content_format: VP9, type: :packetized}
       end
 
-    new_track_info = Map.put(track_info, :active, true)
-    new_tracks = Map.put(tracks, id, new_track_info)
-    new_state = %State{state | tracks: new_tracks}
+    # now that the pad is added all cached buffers destined for this pad can be sent
+    {active, inactive} = Enum.split_with(state.cache, fn {pad_id, _} -> pad_id == id end)
+    actions = [{:caps, {Pad.ref(:output, id), caps}} | output(active)]
+    new_state = %State{state | tracks: activate_track(id, tracks), cache: inactive}
 
-    # now that the pad is added all cached buffers intended for this pad can be sent
-    # TODO Again, all of this should be a single functions that will return cached buffers for given pad and return updated state. You are hand crafting everything which is less readable.
-    to_send = active(state.cache, new_state)
-    new_cache = inactive(state.cache, new_state)
-    final_state = %State{new_state | cache: new_cache}
-    buffer_actions = Enum.map(to_send, &output/1)
-    caps_action = {:caps, {Pad.ref(:output, id), caps}}
-    actions = [caps_action | buffer_actions]
+    {{:ok, actions}, new_state}
+  end
 
-    # IO.puts("    Pad #{id} added. Demuxer sending cached Buffers")
-    {{:ok, actions}, final_state}
+  defp activate_track(id, tracks) do
+    new_track_info = Map.put(tracks[id], :active, true)
+    Map.put(tracks, id, new_track_info)
+  end
+
+  defp output(buffers_list) when is_list(buffers_list) do
+    Enum.map(buffers_list, &output/1)
   end
 
   defp output({track_id, buffers}) do
     {:buffer, {Pad.ref(:output, track_id), buffers}}
-  end
-
-  # returns buffers intended for pads that are currently active
-  defp active(buffers, state) do
-    Enum.filter(buffers, fn {id, _data} -> state.tracks[id].active end)
-  end
-
-  defp inactive(buffers, state) do
-    Enum.filter(buffers, fn {id, _data} -> not state.tracks[id].active end)
   end
 
   defp send_notify_pads(tracks) when is_map(tracks) do
@@ -144,7 +126,6 @@ defmodule Membrane.WebM.Demuxer do
     |> Keyword.get_values(:SimpleBlock)
     |> Enum.map(&prepare_simple_block(&1, cluster[:Timecode]))
     |> List.flatten()
-    # TODO why reverse?
     |> Enum.reverse()
     |> Enum.group_by(& &1.track_number, &packetize/1)
   end
