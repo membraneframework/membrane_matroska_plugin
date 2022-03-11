6bf63afc7db0ff3b136a96874a11c16d7ce50b3d
Maks Stachowiak <maks.stachowiak@swmansion.com>
2022-03-11 15:57:00 +0100

    clean up constructing elements

--- lib/membrane_webm/serializer/elements.ex
+++ lib/membrane_webm/serializer/elements.ex
@@ -1,9 +1,10 @@
 defmodule Membrane.WebM.Serializer.Elements do
-  @moduledoc """
-  Module for constructing the top-level elements constituting a WebM file
-  """
-  alias Membrane.WebM.Parser.Codecs
+  @moduledoc false
 
+  # Module for constructing the top-level elements constituting a WebM file Segment
+  # https://www.ietf.org/archive/id/draft-ietf-cellar-matroska-08.html#section-7
+
+  alias Membrane.WebM.Parser.Codecs
   alias Membrane.WebM.Serializer
   alias Membrane.{Opus, VP8, VP9}
 
@@ -35,46 +36,15 @@ defmodule Membrane.WebM.Serializer.Elements do
   defp construct_track_entry({id, %{caps: %Opus{channels: channels}, track_number: track_number}}) do
     {:TrackEntry,
      [
-       # CodecPrivate is identical to the ogg ID header specified here:
-       # https://datatracker.ietf.org/doc/html/rfc7845#section-5.1
-       # it can be ignored for opus streams with 1 or 2 channels but must be used to correctly play higher-channel-count streams
+       # this field will become important if membrane expands support for opus to more than 2 channels
        CodecPrivate: Codecs.construct_opus_id_header(channels),
        Audio: [
-         # I guess this is hardcoded. Hard to tell from RFC
-         BitDepth: 32,
-         # Possible frequencies: 8, 12, 16, 24, 48 kHz
-         # https://datatracker.ietf.org/doc/html/rfc6716#section-2
-         SamplingFrequency: 2.4e4,
          Channels: channels
        ],
-       # Tracktype: 2 for `audio`
-       # https://www.ietf.org/archive/id/draft-ietf-cellar-matroska-08.html#name-tracktype-values
+       # 2 for audio
        TrackType: 2,
-       # SeekPreRoll: copied over from ffmpeg webm
-       # https://www.ietf.org/archive/id/draft-ietf-cellar-matroska-08.html#name-seekpreroll-element
-       # https://opus-codec.org/docs/opusfile_api-0.5/group__stream__seeking.html
-       SeekPreRoll: 80_000_000,
-       # TODO: How do I determine CodecDelay? copying over from ffmpeg webm for now
-       # https://github.com/GStreamer/gstreamer/blob/5e3bf0fff7205390de56747f950f726b456fc65d/subprojects/gst-plugins-good/gst/matroska/matroska-mux.c#L1898
-       # https://www.ietf.org/archive/id/draft-ietf-cellar-matroska-08.html#section-8.1.4.1.28-1.14
-       # https://datatracker.ietf.org/doc/html/rfc6716#section-2
-       # The LP layer is based on the SILK codec [SILK].  It supports NB, MB,
-       # or WB audio and frame sizes from 10 ms to 60 ms, and requires an
-       # additional 5 ms look-ahead for noise shaping estimation.  A small
-       # additional delay (up to 1.5 ms) may be required for sampling rate
-       # conversion.
-       # gstreamer has codecdelay set to 0
-       #  CodecDelay: 3_250_000,
-       CodecDelay: 0,
        CodecID: "A_OPUS",
-       CodecName: "Opus Audio Codec",
-       # I didn't find information about lacing in the Opus RFC so I assume it's always unlaced
-       # https://www.ietf.org/archive/id/draft-ietf-cellar-matroska-08.html#section-8.1.4.1.12
-       FlagLacing: 0,
-       #  TrackUID is an 8 byte number that MUST NOT be 0
-       #  The value of this Element SHOULD be kept the same when making a direct stream copy to another file.
        TrackUID: id,
-       # The track number as used in the Block Header (using more than 127 tracks is not encouraged, though the design allows an unlimited number).
        TrackNumber: track_number
      ]}
   end
@@ -85,69 +55,37 @@ defmodule Membrane.WebM.Serializer.Elements do
     {:TrackEntry,
      [
        Video: [
-         Colour: [
-           # :half,
-           ChromaSitingVert: 2,
-           # :left_coallocated
-           ChromaSitingHorz: 1
-         ],
-         # :progressive, - no interlacing
-         FlagInterlaced: 2,
          PixelHeight: width,
          PixelWidth: height
        ],
-       DefaultDuration: 16_666_666,
-       # :video,
+       # 1 for video
        TrackType: 1,
-       # :vp8,
        CodecID: "V_VP8",
-       # und = undetermined
-       Language: "und",
-       FlagLacing: 0,
        TrackUID: id,
        TrackNumber: track_number
      ]}
   end
 
-  # TODO: check out if relevant source of values: https://www.webmproject.org/docs/container/#TagBinary
   defp construct_track_entry(
          {id, %{caps: %VP9{width: width, height: height}, track_number: track_number}}
        ) do
     {:TrackEntry,
      [
        Video: [
-         Colour: [
-           # TODO:
-           # VP9 provides the option for the 2 chroma planes (called U and V) to be subsampled in either the horizontal or
-           # vertical direction (or both, or neither).
-           # In profiles 0 and 2, only 4:2:0 format is allowed, which means that chroma is subsampled
-           # page 22 https://storage.googleapis.com/downloads.webmproject.org/docs/vp9/vp9-bitstream-specification-v0.6-20160331-draft.pdf
-           ChromaSitingVert: 0,
-           ChromaSitingHorz: 0
-         ],
-         # :progressive, - no interlacing
-         FlagInterlaced: 2,
          PixelHeight: width,
          PixelWidth: height
        ],
-       # TODO: where did this come from?
-       DefaultDuration: 16_666_666,
-       # :video,
-       TrackType: 1,
-       # :vp8,
-       CodecID: "V_VP9",
-       # und = undetermined
-       Language: "und",
-       FlagLacing: 0,
-       TrackUID: id,
-       TrackNumber: track_number
+      # 1 for video
+      TrackType: 1,
+      CodecID: "V_VP9",
+      TrackUID: id,
+      TrackNumber: track_number
      ]}
   end
 
-  @doc """
-  segment position:
-  https://www.ietf.org/archive/id/draft-ietf-cellar-matroska-08.html#name-segment-position
-  """
+  # explanation of segment position:
+  # https://www.ietf.org/archive/id/draft-ietf-cellar-matroska-08.html#name-segment-position
+  # FIXME: clean up after fixing serializer
   @spec construct_seek_head(list({atom, list})) :: {atom, list}
   def construct_seek_head(elements) do
     seeks =
@@ -189,7 +127,7 @@ defmodule Membrane.WebM.Serializer.Elements do
         MuxingApp: "membrane_webm_plugin-#{@version}",
         # TODO: add options to populate title fields
         Title: "Membrane WebM file",
-        # breaks testing reproducibility:
+        # TODO: add current date: breaks testing reproducibility:
         # DateUTC:
         #   :calendar.datetime_to_gregorian_seconds(:calendar.now_to_datetime(:erlang.timestamp())),
         # hardcoded per RFC
@@ -200,23 +138,6 @@ defmodule Membrane.WebM.Serializer.Elements do
     }
   end
 
-  # TODO: a callback or option should be implemented to supply tag values
+  # TODO: add callback or option to supply tag values
   # https://matroska.org/technical/tagging.html
-  # def construct_tags() do
-  #   {:Tags,
-  #    [
-  #      Tag: [
-  #        SimpleTag: [TagString: "00:00:27.201000000", TagName: "DURATION"],
-  #        SimpleTag: [TagString: "Lavc58.134.100 libopus", TagName: "ENCODER"],
-  #        Targets: []
-  #      ],
-  #      Tag: [
-  #        SimpleTag: [TagString: "Lavf58.76.100", TagName: "ENCODER"],
-  #        SimpleTag: [TagString: "Kevin MacLeod", TagName: "ARTIST"],
-  #        SimpleTag: [TagString: "YouTube Audio Library", TagName: "ALBUM"],
-  #        SimpleTag: [TagString: "Cinematic", TagName: "GENRE"],
-  #        Targets: []
-  #      ]
-  #    ]}
-  # end
 end
