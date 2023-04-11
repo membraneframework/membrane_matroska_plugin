0868c37a1ce3c5d0b3b231e2f2bc670366962c90
Radosław Szuma <radoslawszuma@gmail.com>
2023-04-11 09:31:03 +0200

    Update examples and bump version to 0.2.1

--- examples/muxer_h264.exs
+++ examples/muxer_h264.exs
@@ -18,64 +18,52 @@ defmodule Example do
   @output_file Path.expand("example_h264.mkv")
 
   @impl true
-  def handle_init(_options) do
-    children = [
-      muxer: Membrane.Matroska.Muxer,
-      video_source: %Membrane.Hackney.Source{
-        location: @video_url,
-        hackney_opts: [follow_redirect: true]
-      },
-      video_parser: %Membrane.H264.FFmpeg.Parser{
-        framerate: {30, 1},
-        alignment: :au,
-        attach_nalus?: true
-      },
-      video_payloader: %Membrane.MP4.Payloader.H264{parameters_in_band?: true},
-      audio_source: %Membrane.Hackney.Source{
-        location: @audio_url,
-        hackney_opts: [follow_redirect: true]
-      },
-      audio_encoder: %Membrane.Opus.Encoder{
-        application: :audio,
-        input_caps: %RawAudio{
-          channels: 2,
-          sample_format: :s16le,
-          sample_rate: 48_000
-        }
-      },
-      audio_parser: %Membrane.Opus.Parser{delimitation: :undelimit},
-      file_sink: %Membrane.File.Sink{location: @output_file}
+  def handle_init(_ctx,_options) do
+    structure = [child(:video_source, %Membrane.Hackney.Source{
+      location: @video_url,
+      hackney_opts: [follow_redirect: true]
+    })
+    |> child(      :video_parser, %Membrane.H264.FFmpeg.Parser{
+      framerate: {30, 1},
+      alignment: :au,
+      attach_nalus?: true
+    })
+    |> child(      :video_payloader, %Membrane.MP4.Payloader.H264{parameters_in_band?: true})
+    |> child(      :muxer, Membrane.Matroska.Muxer),
+    child(:audio_source, %Membrane.Hackney.Source{
+      location: @audio_url,
+      hackney_opts: [follow_redirect: true]
+    })
+    |> child(      :audio_encoder, %Membrane.Opus.Encoder{
+      application: :audio,
+      input_stream_format: %RawAudio{
+        channels: 2,
+        sample_format: :s16le,
+        sample_rate: 48_000
+      }
+    })
+    |> child(      :audio_parser, %Membrane.Opus.Parser{delimitation: :undelimit})
+    |> get_child(:muxer)
+    |> child(:file_sink, %Membrane.File.Sink{location: @output_file})
     ]
 
-    links = [
-      link(:video_source)
-      |> to(:video_parser)
-      |> to(:video_payloader)
-      |> to(:muxer),
-      link(:audio_source)
-      |> to(:audio_encoder)
-      |> to(:audio_parser)
-      |> to(:muxer),
-      link(:muxer) |> to(:file_sink)
-    ]
-
-    {{:ok, spec: %ParentSpec{children: children, links: links}, playback: :playing}, %{}}
+    {[spec: structure, playback: :playing], %{}}
   end
 
 
   # Next two functions are only a logic for terminating a pipeline when it's done, you don't need to worry
   @impl true
-  def handle_element_end_of_stream({:file_sink, _}, _ctx, state) do
+  def handle_element_end_of_stream(:file_sink, _pad, _ctx, state) do
     Membrane.Pipeline.terminate(self())
-    {:ok, state}
+    {[], state}
   end
 
-  def handle_element_end_of_stream(_element, _ctx, state) do
-    {:ok, state}
+  def handle_element_end_of_stream(_element, _pad, _ctx, state) do
+    {[], state}
   end
 end
 
-{:ok, pipeline} = Example.start_link()
+{:ok,_supervisor, pipeline} = Example.start_link()
 monitor_ref = Process.monitor(pipeline)
 
 receive do
