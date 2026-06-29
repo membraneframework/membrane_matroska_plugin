d288c92047a692b2d433901015bba4963e6eb1a7
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 11:27:04 +0200

    Set ex_doc dependency to >= 0.40.0

--- mix.exs
+++ mix.exs
@@ -61,7 +61,7 @@ defmodule Membrane.Matroska.Plugin.Mixfile do
       {:membrane_h264_plugin, "~> 0.9.0", only: :test, runtime: false},
       {:membrane_ffmpeg_swresample_plugin, "~> 0.19.0", only: :test, runtime: false},
       # Credo
-      {:ex_doc, "~> 0.40", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.40.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: :dev, runtime: false}
     ]
